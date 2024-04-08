94871a7061633b7c7891cfa7ed19f38c5af54d96
krsjoseph <krspjoseph@gmail.com>
2024-04-08 14:05:20 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,13 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" data-theme="lofi">
             <head>
+                <!-- Google Tag Manager -->
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-PXG87RF6');</script>
+                <!-- End Google Tag Manager -->
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
             <body className="antialiased text-white bg-blue-900">
